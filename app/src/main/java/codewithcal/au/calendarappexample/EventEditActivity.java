653ce255dbb8c27b324384977522c4cab3af1fc8
MKhozinA <m.khozin.ft19@mail.umy.ac.id>
2021-07-04 06:26:57 +0700

    Initial commit

---
+++ app/src/main/java/codewithcal/au/calendarappexample/EventEditActivity.java
@@ -0,0 +1,83 @@
+package codewithcal.au.calendarappexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.TimePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.util.Date;
+
+public class EventEditActivity extends AppCompatActivity
+{
+    private EditText eventNameET;
+    private TextView eventDateTV, eventTimeTV;
+    private int t2hour,t2minute;
+
+    private LocalTime time;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_edit);
+        initWidgets();
+        time = LocalTime.now();
+        eventDateTV.setText("Date: " + CalendarUtils.formattedDate(CalendarUtils.selectedDate));
+        eventTimeTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                        EventEditActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+                                t2hour = hourOfDay;
+                                t2minute = minute;
+                                String time = t2hour + ":" + t2minute;
+                                SimpleDateFormat f24Hours = new SimpleDateFormat(
+                                        "HH:mm"
+                                );
+                                try {
+                                    Date date = f24Hours.parse(time);
+                                    SimpleDateFormat f12Hours = new SimpleDateFormat(
+                                            "hh:mm aa"
+                                    );
+                                    eventTimeTV.setText(f12Hours.format(date));
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },12,0,false
+                );
+                timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                timePickerDialog.updateTime(t2hour,t2minute);
+                timePickerDialog.show();
+            }
+        });
+    }
+
+    private void initWidgets()
+    {
+        eventNameET = findViewById(R.id.eventNameET);
+        eventDateTV = findViewById(R.id.eventDateTV);
+        eventTimeTV = findViewById(R.id.eventTimeTV);
+    }
+
+    public void saveEventAction(View view)
+    {
+        String eventName = eventNameET.getText().toString();
+        Event newEvent = new Event(eventName, CalendarUtils.selectedDate, time);
+        Event.eventsList.add(newEvent);
+        finish();
+    }
+}
